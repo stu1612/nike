e7d4defd97279f16052319d40bba258b6a2a3d9b
Stu Bolderson <stu.bolderson@aol.com>
2023-08-25 15:54:39 +0200

    push to main

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import { default as Hero } from "./Hero";
+import { default as PopularProducts } from "./PopularProducts";
+import { default as CustomerReview } from "./CustomerReview";
+import { default as Footer } from "./Footer";
+import { default as Services } from "./Services";
+import { default as SpecialOffer } from "./SpecialOffer";
+import { default as Subscribe } from "./Subscribe";
+import { default as SuperQuality } from "./SuperQuality";
+import { default as Nav } from "./Nav";
+import { default as Button } from "./Button";
+import { default as ShoeCard } from "./ShoeCard";
+import { default as PopularProductCard } from "./PopularProductCard";
+import { default as ServiceCard } from "./ServiceCard";
+import { default as ReviewCard } from "./ReviewCard";
+
+export {
+  Hero,
+  PopularProducts,
+  CustomerReview,
+  Footer,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Nav,
+  Button,
+  ShoeCard,
+  PopularProductCard,
+  ServiceCard,
+  ReviewCard,
+};
